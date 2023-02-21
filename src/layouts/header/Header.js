f541288011f299d5767243fe9a4d6716baca682e
sshinn0224 <sshinn0224@gmail.com>
2023-02-21 22:20:06 +0900

    결국 metarial ui를 설치 하였따.

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,22 +1,24 @@
 import Menu from "../Menu";
 import './Header.scss'
 import {useState} from "react";
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
     let [searchKey, setSearchKey] = useState("")
-    return <><input type="text" placeholder="검색어를 입력하세요"
+    return <><TextField className="header-search-field" label="Search" variant="outlined" type="text" placeholder="검색어를 입력하세요"
                   onChange={(e)=> {
         e.preventDefault();
         setSearchKey(e.target.value)
 
     }}/>
-    <button onClick={() => alert(searchKey)}>Search</button>
+    <button onClick={() => alert(searchKey)}></button>
     </>
-}
-
-function Login(props) {
-    console.log(props)
-    return <div className={props.className}>Login</div>
 }
 
 function Header() {
@@ -25,13 +27,51 @@ function Header() {
         {id:2 , title:'my-page'}
     ]
 
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }));
+
+
     return <div className="header">
-        <div className="header-flex">
-            <h1>LOGO</h1>
-            <div className="header-center"></div>
-            <Login className="header-login" />
-        </div>
-        <Search />
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                </Toolbar>
+            </AppBar>
+        </Box>
+
         <Menu menuList={menuList}/>
     </div>
 }
