cd2ed410ab2a7fe7c34adc989ffc710138dabcca
sshinn0224 <sshinn0224@gmail.com>
2023-02-22 07:33:43 +0900

    search 영역 개발

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,31 +1,47 @@
 import Menu from "../Menu";
 import './Header.scss'
-import {useState} from "react";
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
+// import {alpha} from "react";
+// import TextField from '@mui/material/TextField';
+import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 
-function Search() {
-    let [searchKey, setSearchKey] = useState("")
-    return <><TextField className="header-search-field" label="Search" variant="outlined" type="text" placeholder="검색어를 입력하세요"
-                  onChange={(e)=> {
-        e.preventDefault();
-        setSearchKey(e.target.value)
-
-    }}/>
-    <button onClick={() => alert(searchKey)}></button>
-    </>
-}
+// function Search() {
+//     let [searchKey, setSearchKey] = useState("")
+//     return <><TextField className="header-search-field" label="Search" variant="outlined" type="text" placeholder="검색어를 입력하세요"
+//                   onChange={(e)=> {
+//         e.preventDefault();
+//         setSearchKey(e.target.value)
+//
+//     }}/>
+//     <button onClick={() => alert(searchKey)}></button>
+//     </>
+// }
 
 function Header() {
     const menuList = [
         {id:1 , title:'search'},
         {id:2 , title:'my-page'}
     ]
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    }));
+
 
     const SearchIconWrapper = styled('div')(({ theme }) => ({
         padding: theme.spacing(0, 2),
@@ -57,7 +73,7 @@ function Header() {
 
     return <div className="header">
         <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
+            <AppBar position="static" color="info">
                 <Toolbar>
                     <Search>
                         <SearchIconWrapper>
